6875abcf6ba9e28b7e44c3b53df440e4d6fff621
MelanX <info@melanx.de>
2021-04-05 22:08:59 +0200

    implement modlist command

---
+++ src/main/java/io/github/noeppi_noeppi/libx/impl/commands/ModListCommand.java
@@ -0,0 +1,55 @@
+package io.github.noeppi_noeppi.libx.impl.commands;
+
+import com.mojang.brigadier.Command;
+import com.mojang.brigadier.context.CommandContext;
+import io.github.noeppi_noeppi.libx.command.CommandUtil;
+import net.minecraft.command.CommandSource;
+import net.minecraft.util.text.*;
+import net.minecraft.util.text.event.ClickEvent;
+import net.minecraft.util.text.event.HoverEvent;
+import net.minecraftforge.fml.ModList;
+import net.minecraftforge.fml.loading.moddiscovery.ModInfo;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class ModListCommand implements Command<CommandSource> {
+
+    private static final HoverEvent COPY_MODLIST = new HoverEvent(HoverEvent.Action.SHOW_TEXT, new TranslationTextComponent("libx.misc.copy_modlist"));
+
+    @Override
+    public int run(CommandContext<CommandSource> context) {
+        boolean extended = !CommandUtil.getArgumentOrDefault(context, "simplified", Boolean.class, false);
+
+        AtomicInteger i = new AtomicInteger();
+        IFormattableTextComponent component = new StringTextComponent("");
+        ModList.get().forEachModContainer((modid, container) -> {
+            if (i.get() != 0) {
+                component.appendString("\n");
+            }
+
+            if (!modid.equalsIgnoreCase("minecraft")) {
+                ModInfo modInfo = (ModInfo) container.getModInfo();
+                StringBuilder builder = new StringBuilder();
+
+                builder.append(modInfo.getDisplayName());
+                modInfo.getConfigElement("authors").ifPresent(authors -> {
+                    builder.append(" (by ");
+                    builder.append(authors.toString().trim());
+                    builder.append(")");
+                });
+                if (extended && !modInfo.getDescription().isEmpty()) {
+                    builder.append(": ");
+                    builder.append(modInfo.getDescription().split("\n")[0].trim());
+                }
+
+                component.append(new StringTextComponent(builder.toString()).mergeStyle(i.get() % 2 == 0 ? Style.EMPTY.setColor(Color.fromInt(0xFFFF00)) : Style.EMPTY.setColor(Color.fromInt(0xFF00F6))));
+                i.getAndIncrement();
+            }
+        });
+
+        component.mergeStyle(Style.EMPTY.setHoverEvent(COPY_MODLIST)
+                .setClickEvent(new ClickEvent(ClickEvent.Action.COPY_TO_CLIPBOARD, component.getString())));
+        context.getSource().sendFeedback(component, false);
+        return 1;
+    }
+}
