75c0b5501154fae5b7c640401990f7b0c8a4a565
noeppi_noeppi <noeppinoeppi@gmail.com>
2021-04-10 17:33:04 +0200

    Remove code that was there for debug

--- src/main/java/io/github/noeppi_noeppi/libx/LibX.java
+++ src/main/java/io/github/noeppi_noeppi/libx/LibX.java
@@ -5,27 +5,21 @@ import io.github.noeppi_noeppi.libx.command.UppercaseEnumArgument;
 import io.github.noeppi_noeppi.libx.crafting.ingredient.EffectIngredient;
 import io.github.noeppi_noeppi.libx.crafting.ingredient.NbtIngredient;
 import io.github.noeppi_noeppi.libx.crafting.ingredient.PotionIngredient;
-import io.github.noeppi_noeppi.libx.event.PlayerFindAmmoEvent;
 import io.github.noeppi_noeppi.libx.impl.TileEntityUpdateQueue;
 import io.github.noeppi_noeppi.libx.impl.commands.CommandsImpl;
 import io.github.noeppi_noeppi.libx.impl.config.ConfigEvents;
 import io.github.noeppi_noeppi.libx.impl.inventory.screen.GenericScreen;
 import io.github.noeppi_noeppi.libx.impl.network.NetworkImpl;
-import io.github.noeppi_noeppi.libx.inventory.BaseItemStackHandler;
 import io.github.noeppi_noeppi.libx.inventory.container.GenericContainer;
 import io.github.noeppi_noeppi.libx.mod.ModX;
 import io.github.noeppi_noeppi.libx.render.ClientTickHandler;
 import net.minecraft.client.gui.ScreenManager;
 import net.minecraft.inventory.container.ContainerType;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.util.ResourceLocation;
-import net.minecraft.util.text.StringTextComponent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.crafting.CraftingHelper;
 import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.event.ServerChatEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
@@ -52,8 +46,6 @@ public class LibX extends ModX {
         MinecraftForge.EVENT_BUS.addListener(TileEntityUpdateQueue::tick);
         MinecraftForge.EVENT_BUS.addListener(CommandsImpl::registerCommands);
         MinecraftForge.EVENT_BUS.register(new ConfigEvents());
-        MinecraftForge.EVENT_BUS.addListener(this::serverChat);
-        MinecraftForge.EVENT_BUS.addListener(this::ammo);
 
         CraftingHelper.register(new ResourceLocation(this.modid, "effect"), EffectIngredient.Serializer.INSTANCE);
         CraftingHelper.register(new ResourceLocation(this.modid, "potion"), PotionIngredient.Serializer.INSTANCE);
@@ -82,21 +74,5 @@ public class LibX extends ModX {
     private void registerContainers(RegistryEvent.Register<ContainerType<?>> event) {
         GenericContainer.TYPE.setRegistryName(new ResourceLocation(this.modid, "generic"));
         event.getRegistry().register(GenericContainer.TYPE);
-    }
-    
-    public void serverChat(ServerChatEvent event) {
-        int size;
-        try {
-            size = Integer.parseInt(event.getMessage());
-        } catch (NumberFormatException e) {
-            size = 12 * 8 + 4;
-        }
-        GenericContainer.open(event.getPlayer(), new BaseItemStackHandler(
-                size, s -> System.out.println("Changed slot: " + s))
-        , new StringTextComponent("Test Container"), new ResourceLocation("libx", "test"));
-    }
-    
-    public void ammo(PlayerFindAmmoEvent event) {
-        event.setAmmo(new ItemStack(Items.SPECTRAL_ARROW));
     }
 }
