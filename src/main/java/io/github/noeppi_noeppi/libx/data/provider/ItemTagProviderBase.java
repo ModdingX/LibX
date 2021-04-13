0cf3b17e96c4e9bc078fec1da42afac1530a54b8
noeppi_noeppi <noeppinoeppi@gmail.com>
2021-04-13 21:00:36 +0200

    Some breaking changes to the block and item tag providers. They use the setup layout now like any other libx base provider. This should not be a problem to end users as the classes are only used during datagen. Also some more utility functions for the recipe provider base

--- src/main/java/io/github/noeppi_noeppi/libx/data/provider/ItemTagProviderBase.java
+++ src/main/java/io/github/noeppi_noeppi/libx/data/provider/ItemTagProviderBase.java
@@ -1,16 +1,21 @@
 package io.github.noeppi_noeppi.libx.data.provider;
 
 import io.github.noeppi_noeppi.libx.mod.ModX;
+import net.minecraft.block.Block;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.ItemTagsProvider;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
 import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.registries.ForgeRegistries;
 
 import javax.annotation.Nonnull;
+import java.util.Objects;
 
 /**
  * A base class for block tag provider
  */
-public class ItemTagProviderBase extends ItemTagsProvider {
+public abstract class ItemTagProviderBase extends ItemTagsProvider {
 
     protected final ModX mod;
 
@@ -24,4 +29,42 @@ public class ItemTagProviderBase extends ItemTagsProvider {
     public final String getName() {
         return this.mod.modid + " item tags";
     }
+    
+    @Override
+	protected final void registerTags() {
+        this.setup();
+        
+		ForgeRegistries.ITEMS.getValues().stream()
+				.filter(i -> this.mod.modid.equals(Objects.requireNonNull(i.getRegistryName()).getNamespace()))
+				.filter(i -> !(i instanceof BlockItem))
+				.forEach(this::defaultItemTags);
+
+		ForgeRegistries.ITEMS.getValues().stream()
+				.filter(i -> this.mod.modid.equals(Objects.requireNonNull(i.getRegistryName()).getNamespace()))
+				.filter(i -> i instanceof BlockItem)
+				.map(i -> ((BlockItem) i).getBlock())
+				.forEach(this::defaultBlockItemTags);
+    }
+
+    /**
+     * A method to add your custom tags to items.
+     */
+    protected abstract void setup();
+
+    /**
+     * Called for every item from your mod that is not a {@code BlockItem}. You should
+     * add item tags here, that can be inferred from the item.
+     */
+    public void defaultItemTags(Item item) {
+		
+	}
+
+	/**
+     * Called for every item from your mod that is a {@code BlockItem}. You should
+     * add item tags here, that can be inferred from the block. However this is not
+     * the correct place for block tags.
+     */
+	public void defaultBlockItemTags(Block block) {
+		
+	}
 }
--- src/main/java/io/github/noeppi_noeppi/libx/data/provider/recipe/RecipeProviderBase.java
+++ src/main/java/io/github/noeppi_noeppi/libx/data/provider/recipe/RecipeProviderBase.java
@@ -4,8 +4,11 @@ import io.github.noeppi_noeppi.libx.mod.ModX;
 import net.minecraft.data.*;
 import net.minecraft.util.IItemProvider;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.common.Tags;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.util.Objects;
 import java.util.function.Consumer;
 
 /**
@@ -123,6 +126,129 @@ public abstract class RecipeProviderBase extends RecipeProvider {
                     .setGroup(ingot.asItem().getRegistryName() + "_from_block")
                     .addCriterion("has_item", hasItem(block))
                     .build(consumer, new ResourceLocation(this.mod.modid, ingot.asItem().getRegistryName().getPath() + "_from_block"));
+        }
+    }
+
+    /**
+     * Creates tool recipes for a material. All the tool items may be null in which case the recipe is not created.
+     */
+    public void makeTools(Consumer<IFinishedRecipe> consumer, IItemProvider material, @Nullable IItemProvider sword,
+                          @Nullable IItemProvider axe, @Nullable IItemProvider pick, @Nullable IItemProvider shovel,
+                          @Nullable IItemProvider hoe) {
+        
+        if (sword != null) {
+            ShapedRecipeBuilder.shapedRecipe(sword)
+                    .key('m', material)
+                    .key('s', Tags.Items.RODS_WOODEN)
+                    .patternLine("m")
+                    .patternLine("m")
+                    .patternLine("s")
+                    .setGroup(material.asItem().getRegistryName() + "_sword")
+                    .addCriterion("has_item0", hasItem(Tags.Items.RODS_WOODEN))
+                    .addCriterion("has_item1", hasItem(material))
+                    .build(consumer, new ResourceLocation(this.mod.modid, Objects.requireNonNull(material.asItem().getRegistryName()).getPath() + "_sword"));
+        }
+
+        if (axe != null) {
+            ShapedRecipeBuilder.shapedRecipe(axe)
+                    .key('m', material)
+                    .key('s', Tags.Items.RODS_WOODEN)
+                    .patternLine("mm")
+                    .patternLine("sm")
+                    .patternLine("s ")
+                    .setGroup(material.asItem().getRegistryName() + "_axe")
+                    .addCriterion("has_item0", hasItem(Tags.Items.RODS_WOODEN))
+                    .addCriterion("has_item1", hasItem(material))
+                    .build(consumer, new ResourceLocation(this.mod.modid, Objects.requireNonNull(material.asItem().getRegistryName()).getPath() + "_axe"));
+        }
+
+        if (pick != null) {
+            ShapedRecipeBuilder.shapedRecipe(pick)
+                    .key('m', material)
+                    .key('s', Tags.Items.RODS_WOODEN)
+                    .patternLine("mmm")
+                    .patternLine(" s ")
+                    .patternLine(" s ")
+                    .setGroup(material.asItem().getRegistryName() + "_pick")
+                    .addCriterion("has_item0", hasItem(Tags.Items.RODS_WOODEN))
+                    .addCriterion("has_item1", hasItem(material))
+                    .build(consumer, new ResourceLocation(this.mod.modid, Objects.requireNonNull(material.asItem().getRegistryName()).getPath() + "_pick"));
+        }
+
+        if (shovel != null) {
+            ShapedRecipeBuilder.shapedRecipe(shovel)
+                    .key('m', material)
+                    .key('s', Tags.Items.RODS_WOODEN)
+                    .patternLine("m")
+                    .patternLine("s")
+                    .patternLine("s")
+                    .setGroup(material.asItem().getRegistryName() + "_shovel")
+                    .addCriterion("has_item0", hasItem(Tags.Items.RODS_WOODEN))
+                    .addCriterion("has_item1", hasItem(material))
+                    .build(consumer, new ResourceLocation(this.mod.modid, Objects.requireNonNull(material.asItem().getRegistryName()).getPath() + "_shovel"));
+        }
+
+        if (hoe != null) {
+            ShapedRecipeBuilder.shapedRecipe(hoe)
+                    .key('m', material)
+                    .key('s', Tags.Items.RODS_WOODEN)
+                    .patternLine("mm")
+                    .patternLine("s ")
+                    .patternLine("s ")
+                    .setGroup(material.asItem().getRegistryName() + "_hoe")
+                    .addCriterion("has_item0", hasItem(Tags.Items.RODS_WOODEN))
+                    .addCriterion("has_item1", hasItem(material))
+                    .build(consumer, new ResourceLocation(this.mod.modid, Objects.requireNonNull(material.asItem().getRegistryName()).getPath() + "_hoe"));
+        }
+    }
+
+    /**
+     * Creates armor recipes for a material. All the armor items may be null in which case the recipe is not created.
+     */
+    public void makeArmor(Consumer<IFinishedRecipe> consumer, IItemProvider material, @Nullable IItemProvider helmet,
+                          @Nullable IItemProvider chestplate, @Nullable IItemProvider leggings,
+                          @Nullable IItemProvider boots) {
+        
+        if (helmet != null) {
+            ShapedRecipeBuilder.shapedRecipe(helmet)
+                    .key('m', material)
+                    .patternLine("mmm")
+                    .patternLine("m m")
+                    .setGroup(material.asItem().getRegistryName() + "_helmet")
+                    .addCriterion("has_item", hasItem(material))
+                    .build(consumer, new ResourceLocation(this.mod.modid, Objects.requireNonNull(material.asItem().getRegistryName()).getPath() + "_helmet"));
+        }
+
+        if (chestplate != null) {
+            ShapedRecipeBuilder.shapedRecipe(chestplate)
+                    .key('m', material)
+                    .patternLine("m m")
+                    .patternLine("mmm")
+                    .patternLine("mmm")
+                    .setGroup(material.asItem().getRegistryName() + "_chestplate")
+                    .addCriterion("has_item", hasItem(material))
+                    .build(consumer, new ResourceLocation(this.mod.modid, Objects.requireNonNull(material.asItem().getRegistryName()).getPath() + "_chestplate"));
+        }
+
+        if (leggings != null) {
+            ShapedRecipeBuilder.shapedRecipe(leggings)
+                    .key('m', material)
+                    .patternLine("mmm")
+                    .patternLine("m m")
+                    .patternLine("m m")
+                    .setGroup(material.asItem().getRegistryName() + "_leggings")
+                    .addCriterion("has_item", hasItem(material))
+                    .build(consumer, new ResourceLocation(this.mod.modid, Objects.requireNonNull(material.asItem().getRegistryName()).getPath() + "_leggings"));
+        }
+
+        if (boots != null) {
+            ShapedRecipeBuilder.shapedRecipe(boots)
+                    .key('m', material)
+                    .patternLine("m m")
+                    .patternLine("m m")
+                    .setGroup(material.asItem().getRegistryName() + "_boots")
+                    .addCriterion("has_item", hasItem(material))
+                    .build(consumer, new ResourceLocation(this.mod.modid, Objects.requireNonNull(material.asItem().getRegistryName()).getPath() + "_boots"));
         }
     }
 }
