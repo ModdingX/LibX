2b4b1ad20e7ba524aae835441cb7b2d5d2c520e7
noeppi_noeppi <noeppinoeppi@gmail.com>
2021-04-22 20:15:22 +0200

    Add WorldSeedHolder

---
+++ src/main/java/io/github/noeppi_noeppi/libx/world/WorldSeedHolder.java
@@ -0,0 +1,25 @@
+package io.github.noeppi_noeppi.libx.world;
+
+/**
+ * Provides a way to get the world seed in codecs.
+ */
+public class WorldSeedHolder {
+    
+    private static long seed = 0;
+
+    /**
+     * Gets the world seet to be used in codecs. Vanilla provides no way for this.
+     */
+    public static long getSeed() {
+        return seed;
+    }
+
+    /**
+     * You should never call this. This is therefore marked deprecated.
+     */
+    @Deprecated
+    public static void setSeed(long worldSeed) {
+        System.out.println("World seed: " + worldSeed);
+        seed = worldSeed;
+    }
+}
