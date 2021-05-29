f60d599091374e6f9a1da70c66d9bea644eef915
noeppi_noeppi <noeppinoeppi@gmail.com>
2021-05-29 11:35:04 +0200

    Fixed enum data serializers

--- src/main/java/io/github/noeppi_noeppi/libx/network/datasync/EnumDataSerializer.java
+++ src/main/java/io/github/noeppi_noeppi/libx/network/datasync/EnumDataSerializer.java
@@ -4,30 +4,15 @@ import net.minecraft.network.PacketBuffer;
 import net.minecraft.network.datasync.IDataSerializer;
 
 import javax.annotation.Nonnull;
-import java.util.HashMap;
 
 /**
- * Data serializers for enums.
+ * Data serializers for enums. You need to register it manually tough.
  */
 public class EnumDataSerializer<T extends Enum<T>> implements IDataSerializer<T> {
-
-    private static final HashMap<Class<? extends Enum<?>>, EnumDataSerializer<?>> INSTANCES = new HashMap<>();
-
-    /**
-     * Gets a data serializer for an enum class. The object returned will always be
-     * the same object for the same given class.
-     */
-    public static <T extends Enum<T>> EnumDataSerializer<T> get(Class<T> clazz) {
-        if (!INSTANCES.containsKey(clazz)) {
-            INSTANCES.put(clazz, new EnumDataSerializer<>(clazz));
-        }
-        //noinspection unchecked
-        return (EnumDataSerializer<T>) INSTANCES.get(clazz);
-    }
     
     private final Class<T> enumClass;
 
-    private EnumDataSerializer(Class<T> enumClass) {
+    public EnumDataSerializer(Class<T> enumClass) {
         this.enumClass = enumClass;
     }
 
