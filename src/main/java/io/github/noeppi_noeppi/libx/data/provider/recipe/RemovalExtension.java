247178edc0480dd34b4ea0424363dd769728b678
noeppi_noeppi <noeppinoeppi@gmail.com>
2021-09-11 16:44:19 +0200

    Add possibility to add recipe conditions to a provider

--- src/main/java/io/github/noeppi_noeppi/libx/data/provider/recipe/RemovalExtension.java
+++ src/main/java/io/github/noeppi_noeppi/libx/data/provider/recipe/RemovalExtension.java
@@ -1,47 +1,11 @@
 package io.github.noeppi_noeppi.libx.data.provider.recipe;
 
-import com.google.gson.JsonObject;
 import io.github.noeppi_noeppi.libx.impl.crafting.recipe.EmptyRecipe;
-import net.minecraft.data.recipes.FinishedRecipe;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.item.crafting.RecipeSerializer;
-
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
 
 public interface RemovalExtension extends RecipeExtension {
 
     default void remove(ResourceLocation recipe) {
-        this.consumer().accept(new FinishedRecipe() {
-            
-            @Override
-            public void serializeRecipeData(@Nonnull JsonObject json) {
-                //
-            }
-
-            @Nonnull
-            @Override
-            public ResourceLocation getId() {
-                return recipe;
-            }
-
-            @Nonnull
-            @Override
-            public RecipeSerializer<?> getType() {
-                return EmptyRecipe.Serializer.INSTANCE;
-            }
-
-            @Nullable
-            @Override
-            public JsonObject serializeAdvancement() {
-                return null;
-            }
-
-            @Nullable
-            @Override
-            public ResourceLocation getAdvancementId() {
-                return null;
-            }
-        });
+        this.consumer().accept(EmptyRecipe.empty(recipe));
     }
 }
