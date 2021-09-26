a47c598008c4c1a04f46d03431447d7fa4b9f2df
noeppi_noeppi <noeppinoeppi@gmail.com>
2021-09-26 21:03:37 +0200

    Move setter from WorldSeedHolder into impl package

---
+++ src/main/java/io/github/noeppi_noeppi/libx/impl/world/WorldSeedHolderImpl.java
@@ -0,0 +1,6 @@
+package io.github.noeppi_noeppi.libx.impl.world;
+
+public class WorldSeedHolderImpl {
+
+    public static long seed = 0;
+}
--- src/main/java/io/github/noeppi_noeppi/libx/world/WorldSeedHolder.java
+++ src/main/java/io/github/noeppi_noeppi/libx/world/WorldSeedHolder.java
@@ -2,28 +2,18 @@ package io.github.noeppi_noeppi.libx.world;
 
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.MapCodec;
+import io.github.noeppi_noeppi.libx.impl.world.WorldSeedHolderImpl;
 
 /**
  * Provides a way to get the seed for the current world.
  */
 public class WorldSeedHolder {
-    
-    private static long seed = 0;
 
     /**
      * Gets the world seed to be used in codecs. Vanilla provides no way for this.
      */
     public static long getSeed() {
-        return seed;
-    }
-
-    /**
-     * You should never call this. This is therefore marked deprecated.
-     */
-    @Deprecated
-    @SuppressWarnings("DeprecatedIsStillUsed")
-    public static void setSeed(long worldSeed) {
-        seed = worldSeed;
+        return WorldSeedHolderImpl.seed;
     }
     
     public static MapCodec<Long> fieldOf(String name) {
