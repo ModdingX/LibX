25b320276b986f9662313ae58ba37d3a8256c3f8
MelanX <info@melanx.de>
2021-10-09 22:15:08 +0200

    add javadoc and default handling

--- src/main/java/io/github/noeppi_noeppi/libx/data/provider/SoundDefinitionsProviderBase.java
+++ src/main/java/io/github/noeppi_noeppi/libx/data/provider/SoundDefinitionsProviderBase.java
@@ -3,12 +3,16 @@ package io.github.noeppi_noeppi.libx.data.provider;
 import io.github.noeppi_noeppi.libx.mod.ModX;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.sounds.SoundEvent;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import net.minecraftforge.common.data.SoundDefinition;
 import net.minecraftforge.common.data.SoundDefinitionsProvider;
+import net.minecraftforge.registries.ForgeRegistries;
 
 import javax.annotation.Nonnull;
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * A base class for sound definition provider. Adds methods to add multiple sounds with same name at once.
@@ -16,6 +20,8 @@ import java.util.ArrayList;
 public abstract class SoundDefinitionsProviderBase extends SoundDefinitionsProvider {
 
     protected final ModX mod;
+
+    private final Set<SoundEvent> blacklist = new HashSet<>();
 
     public SoundDefinitionsProviderBase(ModX mod, DataGenerator generator, ExistingFileHelper helper) {
         super(generator, mod.modid, helper);
@@ -28,13 +34,37 @@ public abstract class SoundDefinitionsProviderBase extends SoundDefinitionsProvi
         return this.mod.modid + " sound definitions";
     }
 
+    /**
+     * This sound event will not be processed by the default generator
+     */
+    protected void manualSound(SoundEvent soundEvent) {
+        this.blacklist.add(soundEvent);
+    }
+
     @Override
     public void registerSounds() {
         this.setup();
+
+        for (ResourceLocation id : ForgeRegistries.SOUND_EVENTS.getKeys()) {
+            SoundEvent soundEvent = ForgeRegistries.SOUND_EVENTS.getValue(id);
+            if (soundEvent != null && this.mod.modid.equals(id.getNamespace()) && !this.blacklist.contains(soundEvent)) {
+                this.defaultSoundDefinition(id, soundEvent);
+            }
+        }
     }
 
     protected abstract void setup();
 
+    protected void defaultSoundDefinition(ResourceLocation id, SoundEvent soundEvent) {
+        String subtitle = "subtitle." + id.getNamespace() + "." + id.getPath().replace("/", ".");
+        this.add(id, definition()
+                .with(sound(id))
+                .subtitle(subtitle));
+    }
+
+    /**
+     * The name for the sound with the amount of different sound files.
+     */
     protected Sounds sound(String name, int amount) {
         Sounds sounds = new Sounds();
         for (int i = 0; i < amount; i++) {
@@ -43,6 +73,9 @@ public abstract class SoundDefinitionsProviderBase extends SoundDefinitionsProvi
         return sounds;
     }
 
+    /**
+     * The resource location for the sound with the amount of different sound files.
+     */
     protected Sounds sound(ResourceLocation name, int amount) {
         return this.sound(name, SoundDefinition.SoundType.SOUND, amount);
     }
@@ -55,6 +88,9 @@ public abstract class SoundDefinitionsProviderBase extends SoundDefinitionsProvi
         return sounds;
     }
 
+    /**
+     * A helper class to allow setting e.g. the volume or pitch for all the sounds in the list.
+     */
     protected static class Sounds extends ArrayList<SoundDefinition.Sound> {
 
         public Sounds volume(double volume) {
@@ -80,6 +116,9 @@ public abstract class SoundDefinitionsProviderBase extends SoundDefinitionsProvi
             return this;
         }
 
+        /**
+         * Same as {@link net.minecraftforge.common.data.SoundDefinition.Sound#stream()} but for all sound files.
+         */
         public Sounds streamSounds() {
             return this.stream(true);
         }
@@ -103,6 +142,9 @@ public abstract class SoundDefinitionsProviderBase extends SoundDefinitionsProvi
             return this;
         }
 
+        /**
+         * Puts all sounds in one sound definition.
+         */
         public SoundDefinition asDefinition() {
             SoundDefinition definition = SoundDefinition.definition();
             this.iterator().forEachRemaining(definition::with);
