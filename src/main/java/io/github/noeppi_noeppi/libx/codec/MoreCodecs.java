8bfa9874c726da12c09c199272b1f3a82da7bc0b
noeppi_noeppi <noeppinoeppi@gmail.com>
2021-12-12 12:27:56 +0100

    Serialise forge caps in CraftingHelper2 and remove recipe item stack and safe nbt codecs.

--- src/main/java/io/github/noeppi_noeppi/libx/codec/MoreCodecs.java
+++ src/main/java/io/github/noeppi_noeppi/libx/codec/MoreCodecs.java
@@ -1,20 +1,12 @@
 package io.github.noeppi_noeppi.libx.codec;
 
-import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import com.mojang.datafixers.util.Pair;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.DataResult;
 import com.mojang.serialization.DynamicOps;
-import com.mojang.serialization.codecs.PrimitiveCodec;
-import com.mojang.serialization.codecs.RecordCodecBuilder;
 import io.github.noeppi_noeppi.libx.impl.codec.EnumCodec;
 import io.github.noeppi_noeppi.libx.impl.codec.ForgeRegistryCodec;
-import net.minecraft.nbt.CompoundTag;
-import net.minecraft.nbt.NbtOps;
-import net.minecraft.nbt.TagParser;
 import net.minecraft.util.Unit;
-import net.minecraft.world.item.ItemStack;
-import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.IForgeRegistry;
 import net.minecraftforge.registries.IForgeRegistryEntry;
 
@@ -52,44 +44,4 @@ public class MoreCodecs {
             return DataResult.success(Pair.of(Unit.INSTANCE, input));
         }
     };
-
-    /**
-     * A codec that encodes a {@link CompoundTag} into a string. The {@link CompoundTag} will always be
-     * encoded as a string, even if it is serialised to nbt.
-     * The difference between this and {@link CompoundTag#CODEC} is that this will be stored as a string,
-     * so it can safely be used with any {@link DynamicOps}.
-     * This should be used when it is required to work with {@link DynamicOps} that are no {@link NbtOps}.
-     */
-    public static final Codec<CompoundTag> SAFE_COMPOUND_TAG = new PrimitiveCodec<>() {
-        
-        @Override
-        public <T> DataResult<CompoundTag> read(DynamicOps<T> ops, T input) {
-            return ops.getStringValue(input).flatMap(str -> {
-                try {
-                    return DataResult.success(TagParser.parseTag(str));
-                } catch (CommandSyntaxException e) {
-                    return DataResult.error("Not a compound tag: " + str);
-                }
-            });
-        }
-        
-        @Override
-        public <T> T write(DynamicOps<T> ops, CompoundTag value) {
-            return ops.createString(value.toString());
-        }
-    };
-
-    /**
-     * A codec that encodes an {@link ItemStack} in a format similar to what is used in recipes.
-     * Also this codec uses {@link #SAFE_COMPOUND_TAG} to store nbt.
-     */
-    public static final Codec<ItemStack> RECIPE_ITEM_STACK = RecordCodecBuilder.create(instance -> instance.group(
-            registry(ForgeRegistries.ITEMS).fieldOf("item").forGetter(ItemStack::getItem),
-            Codec.INT.fieldOf("count").orElse(1).forGetter(ItemStack::getCount),
-            SAFE_COMPOUND_TAG.fieldOf("tag").orElseGet(CompoundTag::new).forGetter(ItemStack::getTag)
-    ).apply(instance, instance.stable((item, count, tag) -> {
-        ItemStack stack = new ItemStack(item, count);
-        if (!tag.isEmpty()) stack.setTag(tag);
-        return stack;
-    })));
 }
--- src/main/java/io/github/noeppi_noeppi/libx/crafting/CraftingHelper2.java
+++ src/main/java/io/github/noeppi_noeppi/libx/crafting/CraftingHelper2.java
@@ -1,9 +1,12 @@
 package io.github.noeppi_noeppi.libx.crafting;
 
 import com.google.gson.JsonObject;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.Tag;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.common.crafting.CraftingHelper;
 
+import javax.annotation.Nullable;
 import java.util.Objects;
 
 /**
@@ -15,9 +18,26 @@ public class CraftingHelper2 {
         JsonObject json = new JsonObject();
         json.addProperty("item", Objects.requireNonNull(stack.getItem().getRegistryName(), "Can't serialize ItemStack: Item has no registry name: " + stack.getItem()).toString());
         json.addProperty("count", stack.getCount());
-        if (writeNBT && stack.hasTag()) {
-            json.addProperty("nbt", stack.getOrCreateTag().toString());
+        if (writeNBT) {
+            CompoundTag stackTag = stack.hasTag() ? stack.getTag() : null;
+            Tag capsTag = forgeCaps(stack);
+            if (stackTag != null || capsTag != null) {
+                // Need to make a copy, so the stack is not modified.
+                CompoundTag resultTag = stackTag == null ? new CompoundTag() : stackTag.copy();
+                if (capsTag != null) resultTag.put("ForgeCaps", capsTag);
+                json.addProperty("nbt", resultTag.toString());
+            }
         }
         return json;
+    }
+    
+    @Nullable
+    private static Tag forgeCaps(ItemStack stack) {
+        CompoundTag nbt = stack.serializeNBT();
+        if (nbt.contains("ForgeCaps") && (!(nbt.get("ForgeCaps") instanceof CompoundTag cmp) || !cmp.isEmpty())) {
+            return nbt.get("ForgeCaps");
+        } else {
+            return null;
+        }
     }
 }
