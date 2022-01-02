f925dd08e9b75de289ca4a44fd32d947c472c967
noeppi_noeppi <noeppinoeppi@gmail.com>
2022-01-02 18:28:39 +0100

    EnumCodec fixes

--- src/main/java/io/github/noeppi_noeppi/libx/impl/codec/EnumCodec.java
+++ src/main/java/io/github/noeppi_noeppi/libx/impl/codec/EnumCodec.java
@@ -5,6 +5,7 @@ import com.mojang.serialization.Codec;
 import com.mojang.serialization.DataResult;
 import com.mojang.serialization.DynamicOps;
 import io.github.noeppi_noeppi.libx.codec.CodecHelper;
+import net.minecraftforge.common.IExtensibleEnum;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -15,19 +16,21 @@ public class EnumCodec<A extends Enum<A>> implements Codec<A> {
 
     public static synchronized <A extends Enum<A>> EnumCodec<A> get(Class<A> clazz) {
         //noinspection unchecked
-        return (EnumCodec<A>) INSTANCES.computeIfAbsent(clazz, EnumCodec::new);
+        return (EnumCodec<A>) INSTANCES.computeIfAbsent(clazz, c -> new EnumCodec<>(clazz));
     }
 
     private final Class<A> clazz;
+    private final boolean extensible;
 
     private EnumCodec(Class<A> clazz) {
         if (!clazz.isEnum()) throw new IllegalArgumentException("Can't create enum codec for non-enum class: " + clazz);
         this.clazz = clazz;
+        this.extensible = IExtensibleEnum.class.isAssignableFrom(this.clazz);
     }
 
     @Override
     public <T> DataResult<T> encode(A input, DynamicOps<T> ops, T prefix) {
-        if (ops.compressMaps()) {
+        if (ops.compressMaps() && !this.extensible) {
             return ops.mergeToPrimitive(prefix, ops.createInt(input.ordinal()));
         } else {
             return ops.mergeToPrimitive(prefix, ops.createString(input.name()));
@@ -36,7 +39,7 @@ public class EnumCodec<A extends Enum<A>> implements Codec<A> {
 
     @Override
     public <T> DataResult<Pair<A, T>> decode(DynamicOps<T> ops, T input) {
-        if (ops.compressMaps()) {
+        if (ops.compressMaps() && !this.extensible) {
             return ops.getNumberValue(input)
                     .flatMap(number -> CodecHelper.doesNotThrow(() -> {
                         A[] elems = this.clazz.getEnumConstants();
