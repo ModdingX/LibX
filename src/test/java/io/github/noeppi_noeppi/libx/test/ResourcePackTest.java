2c0552e7d100222b923373a234c11f06ca28ae8e
noeppi_noeppi <noeppinoeppi@gmail.com>
2022-03-21 13:40:42 +0100

    Improve the resource pack test to use SharedConstants. Also add another test to keep the AT file clean.

--- src/test/java/io/github/noeppi_noeppi/libx/test/ResourcePackTest.java
+++ src/test/java/io/github/noeppi_noeppi/libx/test/ResourcePackTest.java
@@ -4,6 +4,7 @@ import com.google.gson.GsonBuilder;
 import com.google.gson.JsonObject;
 import io.github.noeppi_noeppi.libx.LibX;
 import io.github.noeppi_noeppi.libx.impl.datapack.LibXDatapack;
+import net.minecraft.SharedConstants;
 import org.junit.jupiter.api.Test;
 
 import java.io.InputStreamReader;
@@ -12,14 +13,17 @@ import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+// Test the datapack version in pack.mcmeta and the version for dynamic datapacks
 public class ResourcePackTest {
     
     @Test
+    @SuppressWarnings("deprecation")
     public void testPackVersion() throws Throwable {
         try (Reader in = new InputStreamReader(Objects.requireNonNull(LibX.class.getResourceAsStream("/pack.mcmeta"), "pack.mcmeta file not found"))) {
             JsonObject packInfo = new GsonBuilder().create().fromJson(in, JsonObject.class);
             int packVersion = packInfo.get("pack").getAsJsonObject().get("pack_format").getAsInt();
-            assertEquals(packVersion, LibXDatapack.PACK_VERSION, "Dynamic datapack version does not match version set in pack.mcmeta");
+            assertEquals(SharedConstants.DATA_PACK_FORMAT, packVersion, "pack.mcmeta does not match current data version");
+            assertEquals(SharedConstants.DATA_PACK_FORMAT, LibXDatapack.PACK_VERSION, "Dynamic datapack version does not match current data version");
         }
     }
 }
