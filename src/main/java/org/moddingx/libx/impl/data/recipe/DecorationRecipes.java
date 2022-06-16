27740e39660042af7515e8651a9ea61614104e9c
MelanX <info@melanx.de>
2022-06-16 20:08:56 +0200

    add better way to detect type of decorated blocks

--- src/main/java/org/moddingx/libx/impl/data/recipe/DecorationRecipes.java
+++ src/main/java/org/moddingx/libx/impl/data/recipe/DecorationRecipes.java
@@ -2,17 +2,28 @@ package org.moddingx.libx.impl.data.recipe;
 
 import net.minecraft.world.level.block.Block;
 import net.minecraftforge.common.Tags;
+import org.moddingx.libx.base.decoration.DecorationContext;
 import org.moddingx.libx.datagen.provider.recipe.RecipeExtension;
+import org.moddingx.libx.datagen.provider.recipe.StoneCuttingExtension;
 import org.moddingx.libx.impl.base.decoration.blocks.*;
 
 public class DecorationRecipes {
     
     public static void defaultRecipes(Block block, RecipeExtension ext) {
         if (block instanceof DecoratedSlabBlock decorated) {
+            if (ext instanceof StoneCuttingExtension cut && decorated.parent.getContext().baseMaterial() == DecorationContext.BaseMaterial.STONE) {
+                cut.stoneCutting(decorated.parent, decorated, 2);
+            }
             ObjectCraftingBuilder.buildShaped(ext, new Object[]{ decorated, 6, "###", '#', decorated.parent });
         } else if (block instanceof DecoratedStairBlock decorated) {
+            if (ext instanceof StoneCuttingExtension cut && decorated.parent.getContext().baseMaterial() == DecorationContext.BaseMaterial.STONE) {
+                cut.stoneCutting(decorated.parent, decorated);
+            }
             ObjectCraftingBuilder.buildShaped(ext, new Object[]{ decorated, 4, "#  ", "## ", "###", '#', decorated.parent });
         } else if (block instanceof DecoratedWallBlock decorated) {
+            if (ext instanceof StoneCuttingExtension cut && decorated.parent.getContext().baseMaterial() == DecorationContext.BaseMaterial.STONE) {
+                cut.stoneCutting(decorated.parent, decorated);
+            }
             ObjectCraftingBuilder.buildShaped(ext, new Object[]{ decorated, 6, "###", "###", '#', decorated.parent });
         } else if (block instanceof DecoratedFenceBlock decorated) {
             ObjectCraftingBuilder.buildShaped(ext, new Object[]{ decorated, 3, "#s#", "#s#", '#', decorated.parent, 's', Tags.Items.RODS_WOODEN });
