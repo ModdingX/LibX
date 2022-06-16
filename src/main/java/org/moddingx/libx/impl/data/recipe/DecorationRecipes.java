34e47e8bbe928729f519607a51417d3e835ab87d
noeppi_noeppi <noeppinoeppi@gmail.com>
2022-06-16 20:13:52 +0200

    Don't rely on stonecutting extension

--- src/main/java/org/moddingx/libx/impl/data/recipe/DecorationRecipes.java
+++ src/main/java/org/moddingx/libx/impl/data/recipe/DecorationRecipes.java
@@ -1,5 +1,9 @@
 package org.moddingx.libx.impl.data.recipe;
 
+import net.minecraft.advancements.CriterionTriggerInstance;
+import net.minecraft.data.recipes.SingleItemRecipeBuilder;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.level.ItemLike;
 import net.minecraft.world.level.block.Block;
 import net.minecraftforge.common.Tags;
 import org.moddingx.libx.base.decoration.DecorationContext;
@@ -7,22 +11,24 @@ import org.moddingx.libx.datagen.provider.recipe.RecipeExtension;
 import org.moddingx.libx.datagen.provider.recipe.StoneCuttingExtension;
 import org.moddingx.libx.impl.base.decoration.blocks.*;
 
+import java.util.List;
+
 public class DecorationRecipes {
     
     public static void defaultRecipes(Block block, RecipeExtension ext) {
         if (block instanceof DecoratedSlabBlock decorated) {
             if (ext instanceof StoneCuttingExtension cut && decorated.parent.getContext().baseMaterial() == DecorationContext.BaseMaterial.STONE) {
-                cut.stoneCutting(decorated.parent, decorated, 2);
+                stoneCutting(ext, Ingredient.of(decorated.parent), decorated, 2);
             }
             ObjectCraftingBuilder.buildShaped(ext, new Object[]{ decorated, 6, "###", '#', decorated.parent });
         } else if (block instanceof DecoratedStairBlock decorated) {
             if (ext instanceof StoneCuttingExtension cut && decorated.parent.getContext().baseMaterial() == DecorationContext.BaseMaterial.STONE) {
-                cut.stoneCutting(decorated.parent, decorated);
+                stoneCutting(ext, Ingredient.of(decorated.parent), decorated, 1);
             }
             ObjectCraftingBuilder.buildShaped(ext, new Object[]{ decorated, 4, "#  ", "## ", "###", '#', decorated.parent });
         } else if (block instanceof DecoratedWallBlock decorated) {
             if (ext instanceof StoneCuttingExtension cut && decorated.parent.getContext().baseMaterial() == DecorationContext.BaseMaterial.STONE) {
-                cut.stoneCutting(decorated.parent, decorated);
+                stoneCutting(ext, Ingredient.of(decorated.parent), decorated, 1);
             }
             ObjectCraftingBuilder.buildShaped(ext, new Object[]{ decorated, 6, "###", "###", '#', decorated.parent });
         } else if (block instanceof DecoratedFenceBlock decorated) {
@@ -42,5 +48,14 @@ public class DecorationRecipes {
         } else if (block instanceof DecoratedSign.Standing decorated) {
             ObjectCraftingBuilder.buildShaped(ext, new Object[]{ decorated, 3, "###", "###", " s ", '#', decorated.parent, 's', Tags.Items.RODS_WOODEN });
         }
+    }
+    
+    private static void stoneCutting(RecipeExtension ext, Ingredient input, ItemLike output, int amount) {
+        SingleItemRecipeBuilder builder = SingleItemRecipeBuilder.stonecutting(input, output, amount);
+        List<CriterionTriggerInstance> criteria = ext.criteria(input);
+        for (int i = 0; i < criteria.size(); i++) {
+            builder.unlockedBy("has_item" + i, criteria.get(i));
+        }
+        builder.save(ext.consumer(), ext.provider().loc(output, "stonecutting"));
     }
 }
