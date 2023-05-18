0be605dc061a5abb2b7302747cab9a75f562f41a
noeppi_noeppi <noeppinoeppi@gmail.com>
2023-05-18 10:17:31 +0200

    Add meta annotations to @ForMod

--- src/main/java/org/moddingx/libx/annotation/ForMod.java
+++ src/main/java/org/moddingx/libx/annotation/ForMod.java
@@ -3,6 +3,10 @@ package org.moddingx.libx.annotation;
 import net.minecraftforge.fml.common.Mod;
 import org.moddingx.libx.mod.ModX;
 
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
 // Not in the ap source set as it needs the ModX type bound.
 /**
  * Normally ModInit will infer the mod id and mod class from your {@link Mod @Mod}
@@ -11,6 +15,8 @@ import org.moddingx.libx.mod.ModX;
  * package belongs. Any annotation closer to the element subject to code generation
  * will suppress all annotations further away.
  */
+@Retention(RetentionPolicy.SOURCE)
+@Documented
 public @interface ForMod {
     
     Class<? extends ModX> value();
