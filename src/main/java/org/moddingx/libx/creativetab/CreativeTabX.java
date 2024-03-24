93a03000680c6a874b543ac317aa9edd3fd2feb1
MelanX <info@melanx.de>
2024-03-24 13:04:54 +0100

    fix items are in creative tab even if default behaviour is overridden

--- src/main/java/org/moddingx/libx/creativetab/CreativeTabX.java
+++ src/main/java/org/moddingx/libx/creativetab/CreativeTabX.java
@@ -110,7 +110,7 @@ public abstract class CreativeTabX {
         ForgeRegistries.ITEMS.getEntries().stream()
                 .filter(entry -> this.mod.modid.equals(entry.getKey().location().getNamespace()))
                 .map(Map.Entry::getValue)
-                .filter(item -> item.requiredFeatures().isSubsetOf(ctx.features()))
+                .filter(item -> item.isEnabled(ctx.features()))
                 .sorted(order)
                 .flatMap(stacks)
                 .forEach(stack -> ctx.output().accept(stack));
