24924eb759849236541602920bd8719da3b62cb3
MelanX <info@melanx.de>
2026-04-12 09:58:04 +0200

    Fix trapdoor and door block properties

--- src/main/java/org/moddingx/libx/impl/base/decoration/blocks/DecoratedDoorBlock.java
+++ src/main/java/org/moddingx/libx/impl/base/decoration/blocks/DecoratedDoorBlock.java
@@ -1,5 +1,6 @@
 package org.moddingx.libx.impl.base.decoration.blocks;
 
+import net.minecraft.Util;
 import net.minecraft.core.BlockPos;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.flag.FeatureFlagSet;
@@ -7,7 +8,10 @@ import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.DoorBlock;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.NoteBlockInstrument;
+import net.minecraft.world.level.material.PushReaction;
 import org.moddingx.libx.base.decoration.DecoratedBlock;
+import org.moddingx.libx.base.decoration.DecorationMaterial;
 import org.moddingx.libx.registration.Registerable;
 
 import javax.annotation.Nonnull;
@@ -17,7 +21,19 @@ public class DecoratedDoorBlock extends DoorBlock implements Registerable {
     public final DecoratedBlock parent;
 
     public DecoratedDoorBlock(DecoratedBlock parent) {
-        super(parent.getMaterialProperties().blockSetType(), Properties.ofFullCopy(parent));
+        super(parent.getMaterialProperties().blockSetType(), Util.make(() -> {
+                    Properties blockProperties = Properties.ofFullCopy(parent)
+                            .noOcclusion()
+                            .pushReaction(PushReaction.DESTROY);
+                    // we need to check for WOOD since isWood() is true for the non-burnable NETHER_WOOD, too
+                    if (parent.getContext().material() == DecorationMaterial.WOOD) {
+                        blockProperties.instrument(NoteBlockInstrument.BASS)
+                                .strength(3.0f)
+                                .ignitedByLava();
+                    }
+                    return blockProperties;
+                })
+        );
         this.parent = parent;
     }
 
--- src/main/java/org/moddingx/libx/impl/base/decoration/blocks/DecoratedTrapdoorBlock.java
+++ src/main/java/org/moddingx/libx/impl/base/decoration/blocks/DecoratedTrapdoorBlock.java
@@ -1,13 +1,17 @@
 package org.moddingx.libx.impl.base.decoration.blocks;
 
+import net.minecraft.Util;
 import net.minecraft.core.BlockPos;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.flag.FeatureFlagSet;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.TrapDoorBlock;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.NoteBlockInstrument;
 import org.moddingx.libx.base.decoration.DecoratedBlock;
+import org.moddingx.libx.base.decoration.DecorationMaterial;
 import org.moddingx.libx.registration.Registerable;
 
 import javax.annotation.Nonnull;
@@ -17,7 +21,19 @@ public class DecoratedTrapdoorBlock extends TrapDoorBlock implements Registerabl
     public final DecoratedBlock parent;
 
     public DecoratedTrapdoorBlock(DecoratedBlock parent) {
-        super(parent.getMaterialProperties().blockSetType(), Properties.ofFullCopy(parent));
+        super(parent.getMaterialProperties().blockSetType(), Util.make(() -> {
+                    Properties blockProperties = Properties.ofFullCopy(parent)
+                            .noOcclusion()
+                            .isValidSpawn(Blocks::never);
+                    // we need to check for WOOD since isWood() is true for the non-burnable NETHER_WOOD, too
+                    if (parent.getContext().material() == DecorationMaterial.WOOD) {
+                        blockProperties.instrument(NoteBlockInstrument.BASS)
+                                .strength(3.0f)
+                                .ignitedByLava();
+                    }
+                    return blockProperties;
+                })
+        );
         this.parent = parent;
     }
 
